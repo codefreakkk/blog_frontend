00b48c7cff9685de53418d9315dd9fbb0d8bb842
codefreak <harshsaid558@gmail.com>
2023-03-19 23:17:48 +0530

    changes done

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect,  useState } from "react";
+import { React, useEffect, useState } from "react";
 import ExploreNavbar from "../components/ExploreNavbar";
 import frontend from "../assets/images/frontend.png";
 import backend from "../assets/images/backend.png";
@@ -12,17 +12,20 @@ import computer from "../assets/images/computer.png";
 import Footer from "../components/Footer";
 import ExploreHeader from "../components/ExploreHeader";
 import axios from "axios";
+import { NavLink } from "react-router-dom";
 
 function Categories() {
   const [categories, setCategories] = useState([]);
+  const [state, setState] = useState(false);
+
   useEffect(() => {
     axios
       .get("http://localhost:8000/api/v1/getallcategories")
       .then((res) => {
         const data = res.data;
         if (data.status === true) {
-          console.log(data.data);
           setCategories(data.data);
+          setState(true);
         } else {
           alert("Some error occured");
         }
@@ -34,141 +37,165 @@ function Categories() {
     <>
       <ExploreNavbar />
       <ExploreHeader title={"Categories"} />
-      <div className="container cat_container">
-        <div class="row cat_inner">
-          <div class="col-md-4 mb-5 col-sm-6">
-            <div className="cat">
-              <div>
-                <img
-                  src={frontend}
-                  className="pointer"
-                  style={{ height: "50px" }}
-                />
+      {state === true ? (
+        <>
+          <div className="container cat_container">
+            <div class="row cat_inner">
+              <div class="col-md-4 mb-5 col-sm-6">
+                <div className="cat">
+                  <div>
+                    <img
+                      src={frontend}
+                      className="pointer"
+                      style={{ height: "50px" }}
+                    />
+                  </div>
+                  <NavLink to={`/categories/${categories[2]._id}`}>
+                    <div className="cat_title">Frontend</div>
+                  </NavLink>
+                </div>
               </div>
-              <div className="cat_title">Frontend</div>
-            </div>
-          </div>
 
-          <div class="col-md-4 mb-5 col-sm-6">
-            <div className="cat">
-              <div>
-                <img
-                  src={backend}
-                  className="pointer"
-                  style={{ height: "50px" }}
-                />
+              <div class="col-md-4 mb-5 col-sm-6">
+                <div className="cat">
+                  <div>
+                    <img
+                      src={backend}
+                      className="pointer"
+                      style={{ height: "50px" }}
+                    />
+                  </div>
+                  <NavLink to={`/categories/${categories[3]._id}`}>
+                    <div className="cat_title">Backend</div>
+                  </NavLink>
+                </div>
               </div>
-              <div className="cat_title">Backend</div>
-            </div>
-          </div>
 
-          <div class="col-md-4 mb-5 col-sm-6">
-            <div className="cat">
-              <div>
-                <img
-                  src={devops}
-                  className="pointer"
-                  style={{ height: "50px" }}
-                  alt=""
-                  srcset=""
-                />
+              <div class="col-md-4 mb-5 col-sm-6">
+                <div className="cat">
+                  <div>
+                    <img
+                      src={devops}
+                      className="pointer"
+                      style={{ height: "50px" }}
+                      alt=""
+                      srcset=""
+                    />
+                  </div>
+                  <NavLink to={`/categories/${categories[8]._id}`}>
+                    <div className="cat_title">Devops</div>
+                  </NavLink>
+                </div>
               </div>
-              <div className="cat_title">Devops</div>
-            </div>
-          </div>
 
-          <div class="col-md-4 mb-5 col-sm-6">
-            <div className="cat">
-              <div>
-                <img
-                  src={coding}
-                  className="pointer"
-                  style={{ height: "50px" }}
-                  alt=""
-                  srcset=""
-                />
+              <div class="col-md-4 mb-5 col-sm-6">
+                <div className="cat">
+                  <div>
+                    <img
+                      src={coding}
+                      className="pointer"
+                      style={{ height: "50px" }}
+                      alt=""
+                      srcset=""
+                    />
+                  </div>
+                  <NavLink to={`/categories/${categories[1]._id}`}>
+                    <div className="cat_title">Data Structure</div>
+                  </NavLink>
+                </div>
               </div>
-              <div className="cat_title">Data Structure</div>
-            </div>
-          </div>
 
-          <div class="col-md-4 mb-5 col-sm-6">
-            <div className="cat">
-              <div>
-                <img
-                  src={java}
-                  className="pointer"
-                  style={{ height: "50px" }}
-                  alt=""
-                  srcset=""
-                />
+              <div class="col-md-4 mb-5 col-sm-6">
+                <div className="cat">
+                  <div>
+                    <img
+                      src={java}
+                      className="pointer"
+                      style={{ height: "50px" }}
+                      alt=""
+                      srcset=""
+                    />
+                  </div>
+                  <NavLink to={`/categories/${categories[4]._id}`}>
+                    <div className="cat_title">Java</div>
+                  </NavLink>
+                </div>
               </div>
-              <div className="cat_title">Java</div>
-            </div>
-          </div>
 
-          <div class="col-md-4 mb-5 col-sm-6">
-            <div className="cat">
-              <div>
-                <img
-                  src={javascript}
-                  style={{ height: "50px" }}
-                  alt=""
-                  srcset=""
-                  className="pointer"
-                />
+              <div class="col-md-4 mb-5 col-sm-6">
+                <div className="cat">
+                  <div>
+                    <img
+                      src={javascript}
+                      style={{ height: "50px" }}
+                      alt=""
+                      srcset=""
+                      className="pointer"
+                    />
+                  </div>
+                  <NavLink to={`/categories/${categories[5]._id}`}>
+                    <div className="cat_title">Javascript</div>
+                  </NavLink>
+                </div>
               </div>
-              <div className="cat_title">Javascript</div>
-            </div>
-          </div>
 
-          <div class="col-md-4 mb-5 col-sm-6">
-            <div className="cat">
-              <div>
-                <img
-                  src={linux}
-                  className="pointer"
-                  style={{ height: "50px" }}
-                  alt=""
-                  srcset=""
-                />
+              <div class="col-md-4 mb-5 col-sm-6">
+                <div className="cat">
+                  <div>
+                    <img
+                      src={linux}
+                      className="pointer"
+                      style={{ height: "50px" }}
+                      alt=""
+                      srcset=""
+                    />
+                  </div>
+                  <NavLink to={`/categories/${categories[0]._id}`}>
+                    <div className="cat_title">Operating System</div>
+                  </NavLink>
+                </div>
               </div>
-              <div className="cat_title">Operating System</div>
-            </div>
-          </div>
 
-          <div class="col-md-4 mb-5 col-sm-6">
-            <div className="cat">
-              <div>
-                <img
-                  src={database}
-                  style={{ height: "50px" }}
-                  alt=""
-                  srcset=""
-                  className="pointer"
-                />
+              <div class="col-md-4 mb-5 col-sm-6">
+                <div className="cat">
+                  <div>
+                    <img
+                      src={database}
+                      style={{ height: "50px" }}
+                      alt=""
+                      srcset=""
+                      className="pointer"
+                    />
+                  </div>
+                  <NavLink to={`/categories/${categories[6]._id}`}>
+                    <div className="cat_title">Database</div>
+                  </NavLink>
+                </div>
               </div>
-              <div className="cat_title">Database</div>
-            </div>
-          </div>
 
-          <div class="col-md-4 mb-5 col-sm-6">
-            <div className="cat">
-              <div>
-                <img
-                  src={computer}
-                  style={{ height: "50px" }}
-                  alt=""
-                  className="pointer"
-                  srcset=""
-                />
+              <div class="col-md-4 mb-5 col-sm-6">
+                <div className="cat">
+                  <div>
+                    <img
+                      src={computer}
+                      style={{ height: "50px" }}
+                      alt=""
+                      className="pointer"
+                      srcset=""
+                    />
+                  </div>
+                  <NavLink to={`/categories/${categories[7]._id}`}>
+                    <div className="cat_title">Computer Network</div>
+                  </NavLink>
+                </div>
               </div>
-              <div className="cat_title">Computer Network</div>
             </div>
           </div>
-        </div>
-      </div>
-      <Footer />
+          <Footer />
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
